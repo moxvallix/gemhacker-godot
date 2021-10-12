635c794c0544e2e18937606e35b6ef13196c842c
Moxvallix <moxvallix@gmail.com>
2021-10-12 22:51:13 +1030

    feat(materials): implements the input display

--- scripts/global.gd
+++ scripts/global.gd
@@ -2,6 +2,13 @@ extends Node
 
 var state
 
+var input = ["red", "red", "blue", "green", "green", "yellow", "blue", "purple", "purple"]
+var input_round = 0
+
+var recipe_1 = ["red", 2]
+var recipe_2 = ["green", 1]
+var recipe_3 = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	state = "none"
@@ -9,5 +16,6 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if state != "running":
+		input_round = 0
---
+++ scripts/input.gd
@@ -0,0 +1,66 @@
+extends Node2D
+
+export (int) var width
+export (int) var texture_width
+export (int) var texture_scale
+
+export (int) var x_offset
+export (int) var y_offset
+
+var global
+var input
+
+var input_row = []
+
+var materials = [
+	[preload("res://assets/materials/ruby.png"), "red"],
+	[preload("res://assets/materials/topaz.png"), "yellow"],
+	[preload("res://assets/materials/emerald.png"), "green"],
+	[preload("res://assets/materials/sapphire.png"), "blue"],
+	[preload("res://assets/materials/amythest.png"), "purple"],
+]
+
+var input_scene = preload("res://scenes/materials/input.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	global = get_node("/root/Global")
+	input = global.input
+	input_row = make_2d_array()
+	gen_material()
+	spawn_material()
+	pass # Replace with function body.
+
+func make_2d_array():
+	var array = []
+	for col in width:
+		array.append([])
+		for row in materials[0].size():
+			array[col].append(null)
+	return array
+
+func gen_material():
+	var position = 0
+	for gem in input:
+		for mat in materials:
+			if gem == mat[1]:
+				input_row[position] = mat
+		position += 1
+
+func spawn_material():
+	var position = 0
+	for gem in input_row:
+		var gem_scene = input_scene.instance()
+		add_child(gem_scene)
+		gem_scene.get_child(0).set_texture(gem[0])
+		gem_scene.input = gem[1]
+		gem_scene.position = Vector2(pos_to_pixel(position), (y_offset * texture_scale))
+		position += 1
+
+func pos_to_pixel(pos):
+	var x = (pos * texture_width * texture_scale) - (x_offset * texture_scale)
+	return x
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
